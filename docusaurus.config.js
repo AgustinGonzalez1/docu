60804878de0679fc856a4b9aadaa3f6cc12c5de8
Agustin Gonzalez <agustin_.gonzalez_@hotmail.com>
2024-09-22 00:21:43 -0300

    add: favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	baseUrl: "/",
 	onBrokenLinks: "throw",
 	onBrokenMarkdownLinks: "warn",
-	favicon: "img/favicon.ico",
+	favicon: "img/A.jpg",
 	organizationName: "ProfeAgus",
 	projectName: "documentacion-web",
 
